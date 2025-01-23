796c69ab1deef3aaa37316f9f8e3c0ae92578ba4
shivam-pawar-7217 <snshivam205@gmail.com>
2025-01-23 23:52:24 +0530

    Initial commit

---
+++ page.jsx
@@ -0,0 +1,12 @@
+import AnimatedHero from "./components/AnimatedHero"
+import Features from "./components/Features"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-[#1a1a2e]">
+      <AnimatedHero />
+      <Features />
+    </div>
+  )
+}
+
