ed63129422b4d1d3e3a2d94e850f805cbdf2a655
shivam-pawar-7217 <snshivam205@gmail.com>
2025-01-25 17:22:04 +0530

    updated the packages and styles

---
+++ utils/quotes.ts
@@ -0,0 +1,27 @@
+export const quotes = [
+  {
+    text: "The best work happens when passion meets opportunity.",
+    author: "Freelance Philosophy",
+  },
+  {
+    text: "Your next big project is just one connection away.",
+    author: "Digital Success",
+  },
+  {
+    text: "Where talent meets freedom, innovation thrives.",
+    author: "Future of Work",
+  },
+  {
+    text: "Transform your ideas into reality with expert freelancers.",
+    author: "Creative Excellence",
+  },
+  {
+    text: "Building the future of work, one project at a time.",
+    author: "Freelance Revolution",
+  },
+  {
+    text: "Connecting global talent with limitless opportunities.",
+    author: "Borderless Work",
+  },
+]
+
